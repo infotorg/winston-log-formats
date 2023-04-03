6608b921a96c8247198b46557d98c27ccd8e7767
Volodymyr Chumak <volodymyr.chumak@tietoevry.com>
2023-04-03 13:16:49 +0200

    Add unit test for main.js file.

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,15 @@
 const formats = require('../src/main');
 
 describe('Test formats exports', () => {
-  test.each(Object.keys(formats))('format "%s" should be a Function', (format) => {
+  const exportedFormats = Object.keys(formats);
+
+  test.each(exportedFormats)('format "%s" should be a Function', (format) => {
     expect(typeof formats[format]).toBe('function');
+  });
+
+  test('it should export all formats', () => {
+    const expectedFormats = ['axios', 'description', 'filter', 'mask', 'requestId', 'trackId'];
+
+    expect(exportedFormats).toEqual(expectedFormats);
   });
 });
